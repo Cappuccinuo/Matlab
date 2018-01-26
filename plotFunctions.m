cd77a0896420deba0c8d3604e87e3a951287ee84
Yuan Gao <gaoyuanbj@yahoo.com>
2018-01-25 23:50:24 -0500

    plotFunction

--- plotFunctions.m
+++ plotFunctions.m
@@ -1,9 +1,9 @@
 %ezplot('sqrt(n) * log(n)');
 %ezplot('log(log(n))');
-h1 = ezplot('n^0.001', [0, 100, 0, 100]);
+h1 = ezplot('gamma(n + 1)', [0, 100, 0, 100]);
 set(h1, 'Color', 'r');
 hold on;
 %ezplot('gamma(n + 1)');
 %ezplot('n^0.001');
-h2 = ezplot('log(log(n ^ 2))', [0, 100, 0, 100]);
+h2 = ezplot('4^n', [0, 100, 0, 100]);
 set(h2, 'Color', 'b');
